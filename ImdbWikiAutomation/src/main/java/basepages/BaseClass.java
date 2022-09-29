6b0a82268da9666312b63983789a400cc40276a3
sagar1252 <114641159+sagar1252@users.noreply.github.com>
2022-09-29 13:57:36 +0530

    Commiting Project

---
+++ ImdbWikiAutomation/src/main/java/basepages/BaseClass.java
@@ -0,0 +1,27 @@
+package basepages;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BaseClass {
+
+	public static WebDriver driver;
+
+	public BaseClass() {
+		String driverName = Property.getProperty("driver.name");
+		System.setProperty(driverName, Property.getProperty("driver.path"));
+
+		if (driverName.contains("gecko")) {
+			driver = new FirefoxDriver();
+		} else if (driverName.contains("chrome")) {
+			driver = (WebDriver) new ChromeDriver();
+		}
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+	}
+
+}
